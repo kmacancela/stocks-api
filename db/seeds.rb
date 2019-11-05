f2eefc3be4addf65f4b15144b411a032b3d8eaec
Karina Macancela <karymacancela@gmail.com>
2019-11-05 13:44:21 -0500

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 
-# easy = Level.create(difficulty: "Easy", startingValue: 20000.00)
-# medium = Level.create(difficulty: "Medium", startingValue: 10000.00)
-# hard = Level.create(difficulty: "Hard", startingValue: 5000.00)
+easy = Level.create(difficulty: "Easy", startingValue: 20000.00)
+medium = Level.create(difficulty: "Medium", startingValue: 10000.00)
+hard = Level.create(difficulty: "Hard", startingValue: 5000.00)
 
-# alex = Game.create(username: "Alex M.", score: 5000.00, level_id: 1)
-# karina = Game.create(username: "Karina M.", score: 5000.00, level_id: 1)
+alex = Game.create(username: "Alex M.", score: 5000.00, level_id: 1)
+karina = Game.create(username: "Karina M.", score: 5000.00, level_id: 1)
 
-# fredddy = Game.create(username: "Fred B.", score: 1000.00, level_id: 2)
-# tawhid = Game.create(username: "Tawhid", score: 2000.00, level_id: 2)
-#
-# franky = Game.create(username: "Franky", score: 10000.00, level_id: 3)
-# bell = Game.create(username: "Bell", score: 11000.00, level_id: 3)
+fredddy = Game.create(username: "Fred B.", score: 1000.00, level_id: 2)
+tawhid = Game.create(username: "Tawhid", score: 2000.00, level_id: 2)
+
+franky = Game.create(username: "Franky", score: 10000.00, level_id: 3)
+bell = Game.create(username: "Bell", score: 11000.00, level_id: 3)
 
 
 
